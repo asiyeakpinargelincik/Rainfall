5530e88cdceb449a3b62f7627713e44f77d607b9
asiyeakpinar <asiyeakpinar4@gmail.com>
2024-02-15 20:20:11 +0000

    Step Definitions snippets created for first scenario

--- Rainfall/Steps/LimitStepDefs.cs
+++ Rainfall/Steps/LimitStepDefs.cs
@@ -3,9 +3,41 @@
 [Binding]
 public class LimitStepDefs
 {
+    [When(@"User send a GET request is made for an individual station ""(.*)"" for ""(.*)"" parameter with a limit parameter set to (.*)")]
+    public void WhenUserSendAgetRequestIsMadeForAnIndividualStationForParameterWithALimitParameterSetTo(string p0, string rainfall, int p2)
+    {
+        
+        
+        
+    }
+    
+    
     [Then(@"the response status code should be (.*)")]
     public void ThenTheResponseStatusCodeShouldBe(int p0)
     {
-        ScenarioContext.StepIsPending();
+        
+        
+       
     }
+    
+    [Then(@"the API should respond with a list containing no more than (.*) rainfall measurements")]
+    public void ThenTheApiShouldRespondWithAListContainingNoMoreThanRainfallMeasurements(int p0)
+    {
+        
+        
+        
+    }
+    
+    
+
+    [Then(@"each measurement should have (.*) relevant information")]
+    public void ThenEachMeasurementShouldHaveRelevantInformation(int p0)
+    {
+        
+        
+       
+    }
+
+
+    
 }
